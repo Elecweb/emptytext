f837b77206e60aff3f26c63c6096c5bbf507fd9b
Pierre Luce <pluce19@gmail.com>
2017-12-28 15:20:14 +0100

    Change deprecated config for ESM

--- rollup.config.esm.js
+++ rollup.config.esm.js
@@ -1,5 +1,7 @@
 import config from './rollup.config.umd.js';
 import {nameLibrary,PATH_DIST} from './config-library.js'
-config.format = "es";
-config.dest = PATH_DIST+nameLibrary+".esm.js";
+config.output = {
+  format: "es",
+  file: PATH_DIST+nameLibrary+".esm.js"
+}
 export default config;
